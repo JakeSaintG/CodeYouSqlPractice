e64d2919ebdd3c2af06a5c33bc756af2b6fc24cd
Jake St. Germain <jakesaint27@gmail.com>
2024-09-02 20:53:35 -0400

    Update Join example

--- SQLite_Exercises/Exercises/06_JoinEx.sql
+++ SQLite_Exercises/Exercises/06_JoinEx.sql
@@ -27,24 +27,21 @@ SELECT *
 FROM ClientCat a
 RIGHT JOIN CatBreed b ON a.CatBreed = b.BreedNM;
 
--- TODO: FIX THIS STEP \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 -- A LEFT JOIN will bring everything over from the LEFT table, even if there isn't a match on the RIGHT.
 -- This means that rows will appear to have extra NULLs in it if there's a ClientCat that isn't represented in CatBreed.
--- Ex: Milo is listed as a "German Rex" which shouldn't be in the CatBreed table. He will have NULLs where CatBreed data should be.
+-- Ex: Cecil is now listed as "Bombay" which shouldn't be in the CatBreed table. He will have NULLs where CatBreed data should be.
 SELECT *
 FROM ClientCat a
 LEFT JOIN CatBreed b ON a.CatBreed = b.BreedNM;
 
--- TODO: FIX THIS STEP \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 -- An INNER JOIN will only bring in details that match from both the LEFT and RIGHT tables.
--- Ex: Milo won't show up since his breed listed in ClientCat isn't in CatBreed.
+-- Ex: Cecil won't show up since his breed listed in ClientCat isn't in CatBreed.
 SELECT *
 FROM ClientCat a
 INNER JOIN CatBreed b ON a.CatBreed = b.BreedNM;
 
--- TODO: FIX THIS STEP \/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/\/
 -- Just saying JOIN is functionally the same as an INNER JOIN.
--- Ex: Milo won't show up again (poor little guy...).
+-- Ex: Cecil won't show up again (poor little guy...).
 SELECT *
 FROM ClientCat a
 JOIN CatBreed b ON a.CatBreed = b.BreedNM;
