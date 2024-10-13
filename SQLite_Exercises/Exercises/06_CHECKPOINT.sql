382e4f96b562afb8ed9cc5fdd857bf09075450b9
JakeSaintG <jakesaint27@gmail.com>
2024-10-12 20:25:14 -0400

    Refine checkpoint exercises

--- SQLite_Exercises/Exercises/06_CHECKPOINT.sql
+++ SQLite_Exercises/Exercises/06_CHECKPOINT.sql
@@ -1,4 +1,9 @@
--- TODO: checkpoint
+/******************************************************************************************
+Title: Checkpoint Exercise!
 
--- further exercises may require the data at rest to be in a certain state. 
--- Ensure that previous non-select statements have been run or use the controls to do so.
+Details:
+Fix the below queries!
+
+******************************************************************************************/
+
+-- Goal: 
--- SQLite_Exercises/Exercises/13_CHECKPOINT.sql
+++ SQLite_Exercises/Exercises/13_CHECKPOINT.sql
@@ -1,4 +1,32 @@
 -- TODO: checkpoint
 
--- further exercises may require the data at rest to be in a certain state. 
--- Ensure that previous non-select statements have been run or use the controls to do so.
+/*
+start combining stuff:
+
+SELECT 
+    DISTINCT CatColorPrimary AS MainColor
+    ,Count(CatColorPrimary) AS Count
+FROM ClientCat
+GROUP BY CatColorPrimary;
+
+
+Now modify the query above by adding the Max last modified date.
+-- Should see all of them with the same date except orange
+SELECT 
+    DISTINCT CatColorPrimary AS MainColor
+    ,Count(CatColorPrimary) AS Count
+    ,MAX(LastModifiedDTS) AS MostRecentModify
+FROM ClientCat
+GROUP BY CatColorPrimary;
+
+Now order by last modified date with the most recent ones on top:
+-- Should see all of them with the same date except orange
+SELECT 
+    DISTINCT CatColorPrimary AS MainColor
+    ,Count(CatColorPrimary) AS Count
+    ,MAX(LastModifiedDTS) AS MostRecentModify
+FROM ClientCat
+GROUP BY CatColorPrimary;
+-- DESC (descending) qill put the biggest (most recent) entries on top top
+
+*/
