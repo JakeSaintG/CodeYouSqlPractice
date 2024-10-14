a8304af03f6f0c495e3d3f7f05959db183fc7329
JakeSaintG <jakesaint27@gmail.com>
2024-10-13 21:06:24 -0400

    add another distinct exercise

--- SQLite_Exercises/Exercises/08_Distinct.sql
+++ SQLite_Exercises/Exercises/08_Distinct.sql
@@ -6,3 +6,8 @@
 	-- Some people have too many cats...
 SELECT DISTINCT CatOwner AS DistinctOwners
 FROM ClientCat;
+
+-- Maybe we want to show the different primary cat colors in our data.
+SELECT 
+    DISTINCT CatColorPrimary AS DistinctColors
+FROM ClientCat;
