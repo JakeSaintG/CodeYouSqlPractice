e524600d5a7fb445fbc949605d39f47ac4101602
JakeSaintG <jakesaint27@gmail.com>
2024-10-19 01:47:05 -0400

    pr prep

--- SQLite_Exercises/Exercises/18_AlterKeys.sql
+++ SQLite_Exercises/Exercises/18_AlterKeys.sql
@@ -4,20 +4,23 @@ Title: Alter Keys
 Query_Type: write
 
 Details:
+Let's say the Veterinarian table start taking a while to query so you've been tasked with
+adding a PRIMARY KEY to the table.
 
+This is something that can be done in other most SQL technologies with an ALTER statement. 
+Here's an example using T-SQL on SQL Server:
+    ALTER TABLE Veterinarian ADD CONSTRAINT PK_Veterinarian PRIMARY KEY (VetNM, VetID);
+    -- NOTE! This will not work on SQLite!
 
+In SQLite, it's not possible to use ALTER to add a PK to an existing table...but we aren't
+quitters! It is possible to do so with a "migration" of sorts, though.
+
+Migration plan:
+    - Create a new Veterinarian table with the PK.
+    - Copy the data into the new table using the existing data. (INSERT with SELECT)
+    - Once copied, drop the old Veterinarian table. (This will delete the old table)
+    - Rename the new table to just "Veterinarian".
 ******************************************************************************************/
-
-/*
-    Let's say the Veterinarian table start taking a while to query so you've been tasked with
-    adding a PRIMARY KEY to the table.
-*/
-
--- In software like SQL Server, a PRIMARY KEY can be added to an existing table with a statement like this:
-ALTER TABLE Veterinarian ADD CONSTRAINT PK_Veterinarian PRIMARY KEY (VetNM, VetID);
--- NOTE! This will not work on SQLite!!!
-
--- In SQLite, it's not possible to use ALTER to add a PK to an existing table. It is possible to do so with a "migration", though:
 
 -- Create a new table with the PRIMARY KEY but named something slightly different:
 CREATE TABLE Veterinarian_NEW
--- SQLite_Exercises/Exercises/19_Drop.sql
+++ SQLite_Exercises/Exercises/19_Drop.sql
@@ -20,13 +20,3 @@ DROP TABLE CatOwner;
 DROP TABLE CatBreed;
 DROP TABLE IF EXISTS Veterinarian;
 DROP TABLE CatColor;
-
-/*
-Why would we want to DROP a table?
-- Sometimes, during activities like copying large piles of data, the fastest way to do it without a substantial 
-load on the host DB is to use code (Powershell, C#, R, Python) to create a temporary table.
-
-- After the data copy, the data can be sorted from the temp table using either code or DB relations.
-
-- After everything is settled, the temp table can be safely dropped.
-*/
