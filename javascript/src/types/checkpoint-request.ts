da61e22d0a2761720ca48c99d9244a6a30b1484c
JakeSaintG <jakesaint27@gmail.com>
2024-10-25 23:09:58 -0400

    remove Py,JS,&CS files until further notice

--- javascript/src/types/checkpoint-request.ts
+++
@@ -1,9 +0,0 @@
-/*
-    NOTE:
-        I am choosing to use an interface here for simplicity.
-        You may choose to create a Type instead and that's okay!
-        https://www.typescriptlang.org/play/?#example/types-vs-interfaces
-*/
-export interface CheckpointRequest {
-    exerciseCheckpoint: number
-}
